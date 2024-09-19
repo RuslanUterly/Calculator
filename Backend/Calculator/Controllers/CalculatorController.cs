d5fb4b291e9c055c4d977f96401da281998adf88
Ruslan Kholikov <kholikov.rus@mail.ru>
2024-09-19 14:35:41 +0300

    Добавлены endpoints

---
+++ Backend/Calculator/Controllers/CalculatorController.cs
@@ -0,0 +1,78 @@
+﻿using Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Persistance.Models;
+
+namespace Calculator.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CalculatorController(ICalculatorService calculatorService) : ControllerBase
+{
+    private readonly ICalculatorService _calculatorService = calculatorService;
+
+    [HttpGet("add")]
+    public IActionResult Add([FromBody] BaseOperationRequest operand)
+    {
+        var calculate = _calculatorService.Add(operand.A, operand.B);
+
+        return Ok(calculate.Result);
+    }
+
+    [HttpGet("subtract")]
+    public IActionResult Subtract([FromBody] BaseOperationRequest operand)
+    {
+        var calculate = _calculatorService.Subtract(operand.A, operand.B);
+
+        return Ok(calculate.Result);
+    }
+
+    [HttpGet("multiply")]
+    public IActionResult Multiply([FromBody] BaseOperationRequest operand)
+    {
+        var calculate = _calculatorService.Multiply(operand.A, operand.B);
+
+        return Ok(calculate.Result);
+    }
+
+    [HttpGet("divide")]
+    public IActionResult Divide([FromBody] BaseOperationRequest operand)
+    {
+        var calculate = _calculatorService.Divide(operand.A, operand.B);
+
+        if (calculate.IsError)
+            return BadRequest(calculate.ErrorMessage);
+
+        return Ok(calculate.Result);
+    }
+
+    [HttpGet("pow")]
+    public IActionResult Power([FromBody] PowerRequest operation)
+    {
+        var calculate = _calculatorService.Power(operation.BaseValue, operation.Exponent);
+        return Ok(calculate.Result);
+    }
+
+    [HttpGet("sqrt")]
+    public IActionResult Root([FromBody] PowerRequest operation)
+    {
+        var calculate = _calculatorService.Root(operation.BaseValue, operation.Exponent);
+
+        if (calculate.IsError)
+            return BadRequest(calculate.ErrorMessage);
+
+        return Ok(calculate.Result);
+
+    }
+
+    [HttpPost("calculate")]
+    public IActionResult Calculate([FromBody] CalculateRequest operation)
+    {
+        var calculate = _calculatorService.Calculate(operation.Expression);
+
+        if (calculate.IsError)
+            return BadRequest(calculate.ErrorMessage);
+
+        return Ok(calculate.Result);
+    }
+}
